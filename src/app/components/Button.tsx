30022c3d4a458faf0f52f25b1c3c10d7fe4edabf
natheeshkumar.r <natheeshkumar.r@thoughtworks.com>
2022-03-26 10:23:59 +0530

    Add local server

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = () => {
   return (
     <div>
-      <button>App</button>
+      <button>Button</button>
     </div>
   );
 };
---
+++ src/app/ui/Preview.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Button from "../components/Button";
+
+const Preview = () => {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+};
+
+export default Preview;
