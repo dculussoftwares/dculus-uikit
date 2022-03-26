57868952c90e1ffc15059658de04702ea26be68d
natheeshkumar.r <natheeshkumar.r@thoughtworks.com>
2022-03-26 21:24:57 +0530

    Refactor button

--- src/app/ui/Preview.tsx
+++ src/app/ui/Preview.tsx
@@ -4,14 +4,8 @@ import { Button } from "../index";
 const Preview = () => {
   return (
     <div>
+      <p>Button</p>
       <Button />
-      <input type="checkbox" className="checkbox" />
-      <button className="btn">Button</button>
-      <button className="btn btn-primary">Button</button>
-      <button className="btn btn-secondary">Button</button>
-      <button className="btn btn-accent">Button</button>
-      <button className="btn btn-ghost">Button</button>
-      <button className="btn btn-link">Button</button>
     </div>
   );
 };
