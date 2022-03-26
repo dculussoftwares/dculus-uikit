909796646fa65abe3a663bdad3eeb602748a2947
natheeshkumar.r <natheeshkumar.r@thoughtworks.com>
2022-03-26 13:11:16 +0530

    ca

--- src/app/components/Button.tsx
+++ src/app/components/Button.tsx
@@ -4,6 +4,13 @@ const Button = () => {
   return (
     <div className="btn">
       <button>Button</button>
+      <input type="checkbox" className="checkbox" />
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
     </div>
   );
 };
